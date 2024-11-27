2be7b612b5515cd802500e2fa8ab07d60b94569b
jakobdanid01 <jakob.deschauer@gmx.com>
2024-11-27 15:36:34 +0100

    Added wrong note visualisation

--- static/js/script.js
+++ static/js/script.js
@@ -2,35 +2,66 @@ const { Renderer, Stave, StaveNote, Voice, Formatter } = Vex.Flow;
 const notes = ['c/4', 'd/4', 'e/4', 'f/4', 'g/4', 'a/4', 'b/4'];
 let currentNote = '';
 let correctCounter = 0;
+const noteContainer = document.getElementById("note-container");
 
-function renderRandomNote() {
-  const div = document.getElementById("note-container");
-  div.innerHTML = '';
-  const renderer = new Renderer(div, Renderer.Backends.SVG);
+function getRandomNote(previousNote) {
+  newNote = '';
+  do {
+    newNote = notes[Math.floor(Math.random() * notes.length)];
+  } while (newNote == previousNote)
+  return newNote;
+}
+
+function getContext() {
+  const renderer = new Renderer(noteContainer, Renderer.Backends.SVG);
   renderer.resize(500, 500);
-  const context = renderer.getContext();
+  return renderer.getContext();
+}
+
+function getStave(context) {
   const stave = new Stave(10, 40, 400);
   stave.addClef("treble").addTimeSignature("4/4");
   stave.setContext(context).draw();
-
-  currentNote = notes[Math.floor(Math.random() * notes.length)];
-  const staveNote = new StaveNote({ keys: [currentNote], duration: "w"});
-  
-  const voice = new Voice({ num_beats: 4, beat_value: 4 });
-  voice.addTickables([staveNote]);
-  new Formatter().joinVoices([voice]).format([voice], 350);
-  voice.draw(context, stave);
+  return stave;
 }
 
-renderRandomNote();
+function renderNotes(currentNotes) {
+  noteContainer.innerHTML = '';
+  const context = getContext();
+  const stave = getStave(context);
+
+  for (let i = 0; i < currentNotes.length; i++) {
+    if(currentNotes[i]){
+      staveNote = new StaveNote({ keys: [currentNotes[i]], duration: "w"});
+      if(i > 0){
+        staveNote.setStyle({fillStyle: "red", strokeStyle: "red"});
+      }
+      const voice = new Voice({ num_beats: 4, beat_value: 4 });
+      voice.addTickables([staveNote]);
+      new Formatter().joinVoices([voice]).format([voice], 350);
+      voice.draw(context, stave);
+    }
+  }
+}
+
+currentNote = getRandomNote();
+currentNotes = [currentNote];
+
+renderNotes(currentNotes);
 
 const eventSource = new EventSource('/stream_notes');
 eventSource.onmessage = function(event) {
-    const detectedNote = event.data;
-    console.log(`Detected Note: ${detectedNote}`);
-    if (detectedNote === currentNote) {
-        correctCounter++;
-        document.getElementById("correct-counter").textContent = correctCounter;
-        renderRandomNote();
-    }
+  const detectedNote = event.data;
+  playedNote = detectedNote;
+  currentNotes[1] = playedNote;
+  console.log(`Detected Note: ${detectedNote}`);
+  renderNotes(currentNotes);
+  if (detectedNote === currentNote) {
+    correctCounter++;
+    document.getElementById("correct-counter").textContent = correctCounter;
+    currentNote = getRandomNote(currentNote);
+    currentNotes[0] = currentNote;
+    currentNotes[1] = '';
+    renderNotes(currentNotes);
+  }
 };
