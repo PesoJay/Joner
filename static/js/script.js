b4805f66ac561958412c56f309358bc80e908ea6
jakobdanid01 <jakob.deschauer@gmx.com>
2024-11-27 20:22:11 +0100

    Changed notation rendering to abcjs
    Chenged libraries from VexFlow to ABCJS in preparation for adding the Music Notation Generation from Estudio

--- static/js/script.js
+++ static/js/script.js
@@ -1,61 +1,53 @@
-const { Renderer, Stave, StaveNote, Voice, Formatter } = Vex.Flow;
-const notes = ['c/4', 'd/4', 'e/4', 'f/4', 'g/4', 'a/4', 'b/4'];
+const notes = ['C,', '^C,', 'D,', '^D,', 'E,', 'F,', '^F,', 'G,', '^G,', 'A,', '^A,', 'B,', 'C', '^C', 'D', '^D', 'E', 'F', '^F', 'G', '^G', 'A', '^A', 'B'];
 let currentNote = '';
 let correctCounter = 0;
 const noteContainer = document.getElementById("note-container");
 
 function getRandomNote(previousNote) {
-  newNote = '';
+  let newNote = '';
   do {
     newNote = notes[Math.floor(Math.random() * notes.length)];
-  } while (newNote == previousNote)
+  } while (newNote === previousNote);
   return newNote;
 }
 
-function getContext() {
-  const renderer = new Renderer(noteContainer, Renderer.Backends.SVG);
-  renderer.resize(500, 500);
-  return renderer.getContext();
-}
-
-function getStave(context) {
-  const stave = new Stave(10, 40, 400);
-  stave.addClef("treble").addTimeSignature("4/4");
-  stave.setContext(context).draw();
-  return stave;
-}
-
 function renderNotes(currentNotes) {
-  noteContainer.innerHTML = '';
-  const context = getContext();
-  const stave = getStave(context);
+  let abcString = "X:1\nT:Note Practice\nM:4/4\nL:1/1\nK:C\n";
+  abcString += "[";
 
-  for (let i = 0; i < currentNotes.length; i++) {
-    if(currentNotes[i]){
-      staveNote = new StaveNote({ keys: [currentNotes[i]], duration: "w"});
-      if(i > 0){
-        staveNote.setStyle({fillStyle: "red", strokeStyle: "red"});
-      }
-      const voice = new Voice({ num_beats: 4, beat_value: 4 });
-      voice.addTickables([staveNote]);
-      new Formatter().joinVoices([voice]).format([voice], 350);
-      voice.draw(context, stave);
-    }
-  }
+  currentNotes.forEach((note) => {
+    abcString += note;
+  });
+  abcString += "]";
+
+  ABCJS.renderAbc(noteContainer.id, abcString, {
+    add_classes: true,
+    staffwidth: 500
+  });
 }
 
-currentNote = getRandomNote();
-currentNotes = [currentNote];
+function colorWrongNote() {
+  const chord = noteContainer.querySelector('.abcjs-note');
+  chord.childNodes.forEach((childNode) => {
+    if(childNode.dataset.name === currentNotes[1]){
+      childNode.style.fill = "red";
+    }
+  });
+}
 
+currentNote = 'C';
+const currentNotes = [currentNote];
 renderNotes(currentNotes);
 
 const eventSource = new EventSource('/stream_notes');
-eventSource.onmessage = function(event) {
+eventSource.onmessage = function (event) {
   const detectedNote = event.data;
-  playedNote = detectedNote;
+  const playedNote = detectedNote;
   currentNotes[1] = playedNote;
   console.log(`Detected Note: ${detectedNote}`);
   renderNotes(currentNotes);
+  colorWrongNote();
+
   if (detectedNote === currentNote) {
     correctCounter++;
     document.getElementById("correct-counter").textContent = correctCounter;
